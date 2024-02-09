a1ee657215d6c475f3ab7dfcec87c463b364c94c
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-09 11:22:32 +0000

    first commit

---
+++ getData.py
@@ -0,0 +1,8 @@
+userName:str=input("What is you Name? = ");
+userAge:int=input("what is your Age? = ");
+userEdu:str=input("What is your Education? = ");
+userHob:str=input("What is your Hobbies? = ");
+print("My name is =  ",userName )
+print("My age is  = ",userAge )
+print("My Education  is ",userEdu )
+print("My Hobies is =  ",userHob)
